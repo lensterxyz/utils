565a8b82eba12d7e6ebe9302365eddeb5e2c89df
bigint <69431456+bigint@users.noreply.github.com>
2022-10-12 23:02:00 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
         source: "/collect/:match*",
         destination: "https://api.mixpanel.com/:match*",
       },
+      {
+        source: "/rpc/polygon",
+        destination: "https://polygon-mainnet.g.alchemy.com/v2/HHfOFn8jsYguteTVvL0cz4g9aydrbjTV",
+      },
     ];
   },
 };
