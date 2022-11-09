10c6c0360667f96fa127008beda5cd9a6ce2d1c1
bigint <69431456+bigint@users.noreply.github.com>
2022-11-09 16:26:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,17 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  trailingSlash: true,
   async rewrites() {
     return [
       {
         source: '/leafwatch',
         destination: 'https://logs.browser-intake-datadoghq.eu/api/v2/logs'
-      },
-      {
-        source: "/rpc/polygon",
-        destination: "https://polygon-mainnet.g.alchemy.com/v2/HHfOFn8jsYguteTVvL0cz4g9aydrbjTV",
-      },
+      }
     ];
   },
 };
